e1543187429ea0c709e84aaca98e979aca83146f
muffinffff <karen_fjy@outlook.com>
2020-05-29 17:38:10 -0400

    numbers of island

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	test := make(map[int]int)
-	val, ok := test[1]
-	fmt.Println(val, ok)
+	// test := make(map[int]int)
+	// val, ok := test[1]
+	grid := [][]byte{{1, 1, 1, 1, 0}, {1, 1, 0, 1, 0}, {1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
+	result := numIslands(grid)
+	fmt.Println(result)
 }
