fccb72f48ce2ee258c25b2d74feb2e825ecb9aa5
muffinffff <karen_fjy@outlook.com>
2020-06-04 12:07:04 -0400

    lru cache

--- main.go
+++ main.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 )
 
+type Student struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	// test := make(map[int]int)
 	// val, ok := test[1]
-	grid := [][]byte{{1, 1, 1, 1, 0}, {1, 1, 0, 1, 0}, {1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
-	result := numIslands(grid)
-	fmt.Println(result)
+	// result := Student{x: 1}
+	// result := new(Student)
+	// grid := [][]byte{{1, 1, 1, 1, 0}, {1, 1, 0, 1, 0}, {1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}}
+	// result := numIslands(grid)
+	// var pa *Student   // pa == nil
+	// pa = new(Student) // pa == &Student{"", 0}
+	// pa.Name = "Alice"
+	// c := pa
+	// c.Age = 30
+	// fmt.Println(pa)
+	// b := Student{"Bob", 20}
+	name := "hi"
+	age := 99
+	v := Student{Name: name, Age: age}
+	fmt.Println(v)
 }
